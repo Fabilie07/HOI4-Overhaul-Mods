56173253ab2f2bf6bb3d1d8c502446e51f746bb3
Fabilie07 <fabilie.gaming@gmail.com>
2026-06-24 01:22:01 +0200

    Further AI Improvements

--- land overhaul bog/common/defines/tom_inf_defines.lua
+++ land overhaul bog/common/defines/tom_inf_defines.lua
@@ -1,3 +1,6 @@
  -- Support Companies
 NDefines.NMilitary.MAX_DIVISION_SUPPORT_HEIGHT = 10	-- Vanilla: 5
 NDefines.NMilitary.MAX_DIVISION_SUPPORT_WIDTH = 1	-- Vanilla: 1
+
+ -- AI
+NDefines.NAI.DESIRE_USE_XP_TO_UPGRADE_LAND_EQUIPMENT = 2.0	-- Vanilla: 1.0
