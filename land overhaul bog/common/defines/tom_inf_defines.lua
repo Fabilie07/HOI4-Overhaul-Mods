dca63edd0c5a9d7490af78da55963302f666a60c
Fabilie07 <fabilie.gaming@gmail.com>
2026-07-16 20:40:32 +0200

    New Division Designer

--- land overhaul bog/common/defines/tom_inf_defines.lua
+++ land overhaul bog/common/defines/tom_inf_defines.lua
@@ -1,6 +1,9 @@
  -- Support Companies
-NDefines.NMilitary.MAX_DIVISION_SUPPORT_HEIGHT = 10	-- Vanilla: 5
-NDefines.NMilitary.MAX_DIVISION_SUPPORT_WIDTH = 1	-- Vanilla: 1
+NDefines.NMilitary.MAX_DIVISION_SUPPORT_HEIGHT = 6	-- Vanilla: 5
+NDefines.NMilitary.MAX_DIVISION_SUPPORT_WIDTH = 2	-- Vanilla: 1
+NDefines.NMilitary.MAX_REGIMENTAL_SUPPORT_WIDTH = 5	-- Vanilla: 5
+NDefines.NMilitary.MAX_REGIMENTAL_SUPPORT_HEIGHT = 2	-- Vanilla: 1
+NDefines.NMilitary.REGIMENTAL_SUPPORT_REQUIRED_BATTALIONS = { 3, 5 }	-- Vanilla: { 3 }
 
  -- AI
 NDefines.NAI.DESIRE_USE_XP_TO_UPGRADE_LAND_EQUIPMENT = 2.0	-- Vanilla: 1.0
